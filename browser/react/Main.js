3781da88146b5bfd43ed49d108d56fe782e83264
rivermanvr <vrgithub@riversconsulting.com>
2017-03-27 15:06:58 -0400

    got nav to work setup simple products ad users component

--- browser/react/Main.js
+++ browser/react/Main.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
+import Nav from './Nav';
+import Users from './Users';
 
 class Main extends Component {
     render() {
         return  (
             <div className="container">
                 <h3>Acme Users & Products</h3>
-                <nav>
-                    <ul className="nav nav-tabs" role="tablist">
-                        <li className="active"><a href="/api/users">Users</a></li>
-                        <li><a href="/api/products">Products</a></li>
-                    </ul>
-                </nav>
-                <div className='row'>
-                    <div className='col-xs-12 col-sm-12 col-md-12 col-lg-12'>
-                        <h5>Heading</h5>
-                    </div>
-                </div>
+                <Nav />
+                <Users />
             </div>
         )
     }
---
+++ browser/react/Users.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Users extends Component {
+    render() {
+        return (
+            <div className='row'>
+                <div className='col-xs-12 col-sm-12 col-md-12 col-lg-12'>
+                    <h5>Users:</h5>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Users;
