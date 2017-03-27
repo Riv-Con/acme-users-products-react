c83a4ace469f3e1706a24960c0eb025701070d7b
rivermanvr <vrgithub@riversconsulting.com>
2017-03-27 15:08:33 -0400

    adding a margin to body headings

--- browser/react/Products.js
+++ browser/react/Products.js
@@ -5,7 +5,7 @@ class Products extends Component {
         return (
             <div className='row'>
                 <div className='col-xs-12 col-sm-12 col-md-12 col-lg-12'>
-                    <h5>Users:</h5>
+                    <h5 className="bodyHeading">Users:</h5>
                 </div>
             </div>  
         )
