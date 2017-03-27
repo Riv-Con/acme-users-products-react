8b1c3e54b2102eac384ca5c3bea11e4b631f13d8
rivermanvr <vrgithub@riversconsulting.com>
2017-03-26 22:47:37 -0400

    added app.use for /api routes

--- app.js
+++ app.js
@@ -7,11 +7,10 @@ const routes = require( './routes' );
 app.use('/vendor', express.static(path.join(__dirname, 'node_modules')));
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
 app.use('/css', express.static(path.join(__dirname, 'browser/css')));
+app.use('/api', routes);
 
 app.get('/', (req, res, next) => {
     res.sendFile(path.join(__dirname, 'browser/index.html'));
 });
-
-app.use('/', routes);
 
 module.exports = app;
