feaa507e2f05768963134a2a136513ea8440b4c5
rivermanvr <vrgithub@riversconsulting.com>
2017-03-27 17:09:13 -0400

    interfacing frontend with backend

--- browser/react/Main.js
+++ browser/react/Main.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Nav from './Nav';
 import Users from './Users';
 
 class Main extends Component {
     constructor() {
         super();
-        this.state = { view: 'Users'};
+        this.state = { view: 'Users', products: [], users: []};
+    }
+    componentDidMount() {
+        Promise.all([
+            axios.get('/api/users'),
+            axios.get('api/products')
+        ])
+        .then(([users, products]) => {
+            this.setState({ products, users });
+        })
     }
     render() {
-        return  (
+        let dataView;
+        if (this.state.view === 'Users') {
+            dataView = <Users users={ this.state.users } />;
+        } else {
+            dataView = <div>This is the Product View</div>;
+        }
+
+        return (
             <div className="container">
                 <h3>Acme Users & Products</h3>
                 <Nav view={this.state.view} />
-                <Users  />
+                { dataView }
             </div>
         )
     }
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,12 @@ const router = express.Router();
 const db = require( '../db' );
 const model = db.models;
 
+router.get('/users', (req, res, next) => {
+    res.send();
+});
 
+router.get('/products', (req, res, next) => {
+    res.send();
+});
 
 module.exports = router;
