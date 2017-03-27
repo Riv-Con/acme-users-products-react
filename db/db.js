2b9a856d454fc9fd95692bc6e6ab6ae9fae055e8
rivermanvr <vrgithub@riversconsulting.com>
2017-03-26 20:11:24 -0400

    db connection done

--- db/db.js
+++ db/db.js
@@ -1,3 +1,5 @@
-const db = 'nothing yet';
+const Sequelize = require( 'sequelize' );
+const connectDB = process.env.DATABASE_URL || 'postgres://localhost/acme_sql';
+const db = new Sequelize(connectDB, {logging: false});
 
 module.exports = db;
