ac4a55df29538b1e8dacfb92e0447637fb5f3e95
rivermanvr <vrgithub@riversconsulting.com>
2017-03-31 23:40:43 -0400

    react-router code done

--- browser/react/Main.js
+++ browser/react/Main.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
 
 import axios from 'axios';
-import Nav from './Nav';
-import UserList from './UserList';
-import ProductList from './ProductList';
+import NavLink from './NavLink';
 
 class Main extends Component {
     constructor() {
         super();
-        this.state = { view: 'Products', products: [], users: [] };
+        this.state = { products: [], users: [] };
 
         this.onDelete = this.onDelete.bind(this);
-        this.onClick = this.onClick.bind(this);
         this.onSave = this.onSave.bind(this);
     }
 
@@ -37,10 +34,6 @@ class Main extends Component {
         }
     }
 
-    onClick(view) {
-        this.setState({ view })
-    }
-
     onDelete(product) {
         const products = this.state.products.filter( _product => _product.id !== product.id);
         this.setState({ products });
@@ -50,20 +43,26 @@ class Main extends Component {
     }
 
     render() {
-        let userView;
-        if (this.state.view === 'Users') {
-            userView = <UserList users={ this.state.users } />;
-        } else {
-            userView = (
-                <ProductList products={ this.state.products } onDelete={ this.onDelete } onSave={ this.onSave } />
-            )
-        }
-
+        let userLength = this.state.users.length;
+        let productLength = this.state.products.length;
         return (
             <div className="container">
                 <h3>Acme Users & Products</h3>
-                <Nav view={ this.state.view} users={ this.state.users} products={ this.state.products } onClick={ this.onClick } />
-                { userView }
+                <nav>
+                    <ul className="nav nav-tabs" role="tablist">
+                        <li><NavLink to="/users">Users ({ userLength })</NavLink></li>
+                        <li><NavLink to="/products">Products ({ productLength })</NavLink></li>
+                    </ul>
+                </nav>
+                { React.cloneElement(this.props.children,
+                        {
+                            users: this.state.users,
+                            products: this.state.products,
+                            onDelete: this.onDelete,
+                            onSave: this.onSave
+                        }
+                    )
+                }
             </div>
         )
     }
